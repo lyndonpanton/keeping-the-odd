e46807e7f8365d24f0681a57b13892a275599abe
Lyndon Panton <LyndonPanton@gmail.com>
2023-01-16 01:13:13 +0000

    Completed problem 2
    Removed all even numbers from the list
    Printed the contents of the list

--- Exercise13/Exercise13/Program.cs
+++ Exercise13/Exercise13/Program.cs
@@ -6,11 +6,28 @@ namespace Exercise13
     {
         static void Main(string[] args)
         {
+            // getting started
             List<int> integers = new List<int>();
 
+            // problem 1
             for (int i = 1; i <= 10; i++)
             {
                 integers.Add(i);
+            }
+
+            for (int i = 0; i < integers.Count; i++)
+            {
+                Console.WriteLine(integers[i]);
+            }
+
+            // problem 2
+            for (int i = integers.Count - 1; i >= 0; i--)
+            {
+                // Console.WriteLine(integers[i]);
+                if (integers[i] % 2 == 0)
+                {
+                    integers.RemoveAt(i);
+                }
             }
 
             for (int i = 0; i < integers.Count; i++)
